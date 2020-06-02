cc3a200141e656129adc4f4bc8a834aa714b1b97
Jer1ah <jeriah97bowers@yahoo.com>
2020-06-02 17:00:27 -0400

    adding navigation functionality

--- js/index.js
+++ js/index.js
@@ -2,12 +2,17 @@
 const navigationController = (function() {
     const _navButton = document.querySelector(".navigation__button");
     const _navBackground = document.querySelector(".navigation__background");
+    const _navList = document.querySelector(".navigation__list");
 
     _navButton.addEventListener("click", () => {
-        if(window.getComputedStyle(_navBackground).transform === "none" || window.getComputedStyle(_navBackground).transform === "matrix(0, 0, 0, 0, 0, 0)") {
+        if(window.getComputedStyle(_navList).visibility === "hidden") {
             _navBackground.style.transform = "scale(80)";
+            _navList.style.visibility = "visible";
+            _navList.style.opacity = "1";
         } else {
             _navBackground.style.transform = "scale(0)";
+            _navList.style.visibility = "hidden";
+            _navList.style.opacity = "0";
         }
     });
 }())
